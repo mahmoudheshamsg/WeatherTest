8c91c0da3c3ab528f2f7bdf3960d5d3ebc78caf7
mahmoudheshamsg <mahmoudheshamsg7@gmail.com>
2025-07-17 17:57:03 +0300

    Basic Implementation Needs some Improvements

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
+func KelvinToCelsius(Kelvin float64) float64 {
+	return Kelvin - 273.15
+}
+func GetWeather(city string) float64 {
+	//JSON struct template to get the temperature from the API
+	var data struct {
+		Main struct {
+			Temp float64 `json:"temp"`
+		} `json:"main"`
+	}
+	//Making an API call to get the weather with the city name and APIKEY
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, os.Getenv("OPENWEATHERMAPAPIKEY"))
+	resp, err := http.Get(url)
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	//Reading the entire JSON at once
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading body: %v\n", err)
 
 	}
-	fmt.Println(os.Getenv("OPENWEATHERMAPAPIKEY"))
+	//decoding the JSON and extracting only the temp value
+	json.Unmarshal(body, &data)
+	return KelvinToCelsius(data.Main.Temp)
+}
+func main() {
+	city := flag.String("city", "Cairo", "Name of the City")
+	flag.Parse()
+	//load .env
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading dotenv file", err)
+	}
+	fmt.Printf("%0.1f°C\n", GetWeather(*city))
 }
